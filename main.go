d74dcb8652bad184f93ec218c1efb442e72534dd
codenamerush <aarushi@rapido.bike>
2021-06-05 20:51:33 +0530

    returning multiple values from a function

--- main.go
+++ main.go
@@ -1,6 +1,11 @@
 package main
 
+import "fmt"
+
 func main() {
 	cards := newDeck()
-	cards.print()
+	hand, remainingDeck := deal(cards, 5)
+	hand.print()
+	fmt.Println("----")
+	remainingDeck.print()
 }
